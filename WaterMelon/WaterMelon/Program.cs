9ba0bb6e8ac4aa7b7222a657206498ba671794e1
Md.Didarul Islam <didaricst@gmail.com>
2024-02-03 00:35:38 +0600

    2024-02-012

---
+++ WaterMelon/WaterMelon/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WaterMelon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MelonWeight = int.Parse(Console.ReadLine());
+            if (MelonWeight % 2 == 0 && MelonWeight != 2)
+            {
+                Console.WriteLine("YES");
+            }
+            else 
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
---
+++ WordCapitalization/WordCapitalization/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WordCapitalization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string TargetWord = Console.ReadLine();
+            string Output = "";
+            if (TargetWord.Length <= 1000 )
+            {
+                Output = TargetWord.Substring(0, 1).ToUpper()+ TargetWord.Substring(1);
+                Console.WriteLine(Output);
+            }
+            else 
+            {
+                Console.WriteLine("Please Check Word! maximum limit 1000");
+            }
+        }
+    }
+}
