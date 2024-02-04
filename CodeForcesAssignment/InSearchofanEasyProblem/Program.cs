43914f9ef842b7b4dc883703d428f56ba4eb18f3
Md.Didarul Islam <didaricst@gmail.com>
2024-02-04 21:51:36 +0600

    11

---
+++ CodeForcesAssignment/InSearchofanEasyProblem/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace InSearchofanEasyProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Persons = int.Parse(Console.ReadLine());
+            int Result = 0;
+            string opinions = Console.ReadLine();
+            string _opinions = string.Empty;
+            foreach (char opinion in opinions.Trim())
+            {
+                if (opinion == ' ')
+                {
+
+                }
+                else
+                {
+                    _opinions = _opinions + opinion;
+                }
+            }
+            int loop = 1;
+            foreach (char opinion in _opinions.Trim())
+            {
+                string Value = opinion.ToString();
+                if (loop <= Persons)
+                {
+                    if (opinion == '0')
+                    {
+
+                        Result += Convert.ToInt32(opinion.ToString());
+                    }
+                    else
+                    {
+                        Result += Convert.ToInt32(opinion.ToString());
+                    }
+                }
+                loop++;
+            }
+            if (Result != 0)
+            {
+                Console.WriteLine("HARD");
+            }
+            else
+            {
+                Console.WriteLine("EASY");
+            }
+
+
+        }
+    }
+}
