4e8c4503dd4a743dd3469a054fee3b40d0c93ce2
Md.Didarul Islam <64219661+Me-Didar@users.noreply.github.com>
2024-02-04 18:12:59 +0600

    520A - Pangram-1

---
+++ CodeForcesAssignment/Pangram/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Pangram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Lenght = int.Parse(Console.ReadLine());
+            string Words = Console.ReadLine().ToUpper();         
+            string Alphabet = string.Empty;
+            foreach (char Word in Words.Trim())
+            {
+                if (Alphabet.Contains(Word.ToString()))
+                {
+
+                }
+                else 
+                {
+                    Alphabet = Alphabet + Word.ToString();
+                }
+            }
+            if (Alphabet.Length == 26)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+        }
+
+    }
+}
---
+++ CodeForcesAssignment/Word/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Words = Console.ReadLine();
+            int UpperCase = 0;
+            int LowerCase = 0;
+            foreach (char Word in Words.Trim())
+            {
+                if (char.IsUpper(Word))
+                {
+                    UpperCase++;
+                }
+                else
+                {
+                    LowerCase++;
+                }
+
+            }
+            if (LowerCase > UpperCase)
+            {
+                Console.WriteLine(Words.ToLower());
+            }
+            else if (LowerCase < UpperCase)
+            {
+                Console.WriteLine(Words.ToUpper());
+            }
+            else
+            {
+                Console.WriteLine(Words.ToLower());
+            }
+          
+        }
+    }
+}
