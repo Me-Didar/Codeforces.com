17dfd1e2f52634511b4ef9e940700781fe32b85b
Md.Didarul Islam <64219661+Me-Didar@users.noreply.github.com>
2024-02-05 15:15:07 +0600

    A - Elephant

---
+++ CodeForcesAssignment/Elephant/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Elephant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int HouseDistance, Steps = 0;
+            int[] Positions = { 5, 4, 3, 2, 1 };
+            HouseDistance = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 5; i++)
+            {
+              //  int test= HouseDistance / Positions[i];
+                Steps += HouseDistance / Positions[i];
+              //  int test2 = HouseDistance % Positions[i];
+                HouseDistance = HouseDistance % Positions[i];
+            }
+
+            Console.WriteLine(Steps);
+        }
+    }
+}
