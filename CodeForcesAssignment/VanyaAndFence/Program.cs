6123375fe3c6582fbae9b3e2400f3376e78cec6f
Md.Didarul Islam <64219661+Me-Didar@users.noreply.github.com>
2024-02-05 17:48:51 +0600

    677A - Vanya and Fence

---
+++ CodeForcesAssignment/VanyaAndFence/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VanyaAndFence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] PersonsFences = Console.ReadLine().Split(' '); //Combination of Person and Fence       
+            int Fence = int.Parse(PersonsFences[1]); //height of the fence
+            string[] HeightPersons = Console.ReadLine().Split(' '); //split by space(All Person height)
+            int width = 0;
+            foreach (var Person in HeightPersons)
+            {
+                var Friend = int.Parse(Person);
+                width += Friend > Fence ? 2 : 1;
+            }
+            Console.WriteLine(width);
+        }
+    }
+}
