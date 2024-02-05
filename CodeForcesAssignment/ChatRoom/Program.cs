06e383abca77555e1a436297396c073adf6407c4
Md.Didarul Islam <64219661+Me-Didar@users.noreply.github.com>
2024-02-05 17:12:35 +0600

    ChatRoom

--- CodeForcesAssignment/ChatRoom/Program.cs
+++ CodeForcesAssignment/ChatRoom/Program.cs
@@ -6,7 +6,63 @@ namespace ChatRoom
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string Message, target;
+            Message = Console.ReadLine();
+            target = "hello"; //------------- To Compare with Input Meassage        
+            int MessageCharSize = Message.Length;
+            int k = 0;
+            int TargetSerial = 0;
+            char LastChar = '\0';
+            char NextChar = '\0';          
+            for (int i = 0; i < MessageCharSize; i++)
+            {
+                //---
+                string Running = Message[i].ToString();
+                string _LastChar = LastChar.ToString();
+                string _NextChar = NextChar.ToString();
+                if (target.Contains(Message[i].ToString())) //-------- Checking Input Char is part of "Hello"
+                {
+                    if (k == 5)
+                    {
+                        break;
+                    }
+
+                    //if (LastChar != Message[i]) //------- Check for Duplicate Char Compare for (ahhellllloou)
+                    //{
+                    //---- Message[i]  Means first Char of Message string
+                    NextChar = target[TargetSerial];
+                    if (Message[i] == NextChar)//----- target[n] Target String char
+                    {
+                        k++;//--------- Increase index to get next  char of Target
+                        LastChar = NextChar;
+                        TargetSerial++;                      
+                    }
+                    else
+                    {
+                        LastChar = Message[i];
+                    }
+                    //}
+                    //else
+                    //{
+                    //    LastChar = Message[i];
+                    //}
+
+                }
+                else
+                {
+                    LastChar = Message[i];
+                }
+            }
+
+
+            if (k == target.Length)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
         }
     }
 }
