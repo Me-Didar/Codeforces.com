d2f340755de3af98d889118575364453118652ca
Md.Didarul Islam <didaricst@gmail.com>
2024-02-14 00:31:18 +0600

    BeautifulMatrix

---
+++ CodeForcesAssignment/BeautifulMatrix/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BeautifulMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] Matrix = Console.ReadLine().Split(' ');  
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
