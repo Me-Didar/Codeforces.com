24eb09436cec0f1618badd2c6ef5b39e2a87911b
Md.Didarul Islam <64219661+Me-Didar@users.noreply.github.com>
2024-02-14 14:47:04 +0600

    BeautifulMatrix V2

--- CodeForcesAssignment/BeautifulMatrix/Program.cs
+++ CodeForcesAssignment/BeautifulMatrix/Program.cs
@@ -6,10 +6,29 @@ namespace BeautifulMatrix
     {
         static void Main(string[] args)
         {
+            bool shouldBreak = false;
+            int Result = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+                string Matrix = Console.ReadLine();
+                string[] s1values = Matrix.Split(' ');
 
-            string[] Matrix = Console.ReadLine().Split(' ');  
-            
-            Console.WriteLine("Hello World!");
+                for (int j = 1; j <= 5; j++)
+                {
+                    int x = int.Parse(s1values[j - 1]);  
+                    if (x == 1)
+                    {
+                        Result = Math.Abs(i - 3) + Math.Abs(j - 3);                     
+                        shouldBreak = true;
+                    }
+                }
+                if (shouldBreak)
+                {
+                    // Break from the outer loop
+                    break;
+                }
+            }    
+         Console.WriteLine(Result.ToString());
         }
     }
 }
